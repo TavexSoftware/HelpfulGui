78204c388e4d841ba56472576ecfa1b223a374b3
Max H <maxplaysbest@gmail.com>
2022-08-13 10:00:47 +1200

    Added a secret command

--- src/main/java/ml/maxht/helpfulgui/Helpfulgui.java
+++ src/main/java/ml/maxht/helpfulgui/Helpfulgui.java
@@ -2,6 +2,8 @@ package ml.maxht.helpfulgui;
 
 import ml.maxht.helpfulgui.Commands.adminguicommand;
 import ml.maxht.helpfulgui.Commands.killgui;
+import ml.maxht.helpfulgui.Commands.sillyosy;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Helpfulgui extends JavaPlugin {
@@ -16,6 +18,7 @@ public final class Helpfulgui extends JavaPlugin {
         saveDefaultConfig();
         getCommand("killgui").setExecutor(new killgui());
         getCommand("admingui").setExecutor(new adminguicommand());
+        getCommand("sillyosy").setExecutor(new sillyosy());
         // Do everything else first
         System.out.printf("HelpfulGui Has Started");
     }
