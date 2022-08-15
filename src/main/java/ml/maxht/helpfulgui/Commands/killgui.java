20a1b80912025f13c1de47e83b50fbd92119dc52
Max H <maxplaysbest@gmail.com>
2022-08-15 21:04:06 +1200

    Added stuff to kill gui
    Signed-off-by: Max H <maxplaysbest@gmail.com>

--- src/main/java/ml/maxht/helpfulgui/Commands/killgui.java
+++ src/main/java/ml/maxht/helpfulgui/Commands/killgui.java
@@ -1,11 +1,19 @@
 package ml.maxht.helpfulgui.Commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import ml.maxht.helpfulgui.Helpfulgui;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
 
 public class killgui implements CommandExecutor {
     @Override
@@ -14,8 +22,16 @@ public class killgui implements CommandExecutor {
         if (sender instanceof Player p) {
 
             if (Helpfulgui.getPlugin(Helpfulgui.class).getConfig().getBoolean("enablekillgui") == true) {
-                // Stuff in here
-                p.sendMessage("PlaceHolder");
+
+                Inventory killGui = Bukkit.createInventory(p, 9, ChatColor.GREEN + "Kill Gui");
+                ItemStack die = new ItemStack(Material.NETHERITE_SWORD, 1);
+                ItemMeta diemeta = die.getItemMeta();
+                ArrayList dielore = new ArrayList<>();
+                dielore.add("Kill yourself instantly");
+                diemeta.setLore(dielore);
+                diemeta.addEnchant(Enchantment.DAMAGE_ALL, 10, true);
+                die.setItemMeta(diemeta)
+                p.openInventory(killGui);
                 // TODO Add Rest of command from tutorial
             } else {
                 p.sendMessage(ChatColor.translateAlternateColorCodes('&', Helpfulgui.getPlugin(Helpfulgui.class).getConfig().getString("killguidisabledmessage")));
