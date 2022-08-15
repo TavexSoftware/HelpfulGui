adf974c7d058eb8b9599508dc8f56324ea234d86
Max H <maxplaysbest@gmail.com>
2022-08-16 08:27:28 +1200

    Did stuff
    Signed-off-by: Max H <maxplaysbest@gmail.com>

--- src/main/java/ml/maxht/helpfulgui/Commands/killgui.java
+++ src/main/java/ml/maxht/helpfulgui/Commands/killgui.java
@@ -23,42 +23,60 @@ public class killgui implements CommandExecutor {
 
             if (Helpfulgui.getPlugin(Helpfulgui.class).getConfig().getBoolean("enablekillgui") == true) {
 
-                Inventory killGui = Bukkit.createInventory(p, 9, ChatColor.GREEN + "Kill Gui");
-                ItemStack die = new ItemStack(Material.NETHERITE_SWORD, 1);
-                ItemMeta diemeta = die.getItemMeta();
-                ArrayList dielore = new ArrayList<>();
-                dielore.add("Kill yourself instantly");
-                diemeta.setLore(dielore);
-                diemeta.addEnchant(Enchantment.DAMAGE_ALL, 10, true);
-                diemeta.setDisplayName(ChatColor.RED + "Die");
-                die.setItemMeta(diemeta);
-                killGui.setItem(1, die);
-                ItemStack fill = new ItemStack(Material.GRAY_STAINED_GLASS_PANE, 1);
-                ItemMeta fillmeta = fill.getItemMeta();
-                ArrayList filllore = new ArrayList<>();
-                filllore.add("There is nothing here!");
-                fillmeta.setLore(filllore);
-                fillmeta.setDisplayName(ChatColor.YELLOW + "There is nothing here!");
-                fill.setItemMeta(fillmeta);
-                killGui.setItem(2, fill);
-                killGui.setItem(3, fill);
-                killGui.setItem(4, fill);
-                killGui.setItem(5, fill);
-                killGui.setItem(6, fill);
-                killGui.setItem(7, fill);
-                killGui.setItem(8, fill);
-                killGui.setItem(9, fill);
+                if (Helpfulgui.getPlugin(Helpfulgui.class).getConfig().getBoolean("killguiperms") == true) {
+                    if (p.hasPermission("helpfulgui.kill")) {
+                        Inventory killGui = Bukkit.createInventory(p, 9, ChatColor.GREEN + "Kill Gui");
+                        ItemStack die = new ItemStack(Material.NETHERITE_SWORD, 1);
+                        ItemMeta diemeta = die.getItemMeta();
+                        ArrayList dielore = new ArrayList<>();
+                        dielore.add("Kill yourself instantly");
+                        diemeta.setLore(dielore);
+                        diemeta.addEnchant(Enchantment.DAMAGE_ALL, 10, true);
+                        diemeta.setDisplayName(ChatColor.RED + "Die");
+                        die.setItemMeta(diemeta);
+                        ItemStack fill = new ItemStack(Material.GRAY_STAINED_GLASS_PANE, 1);
+                        ItemMeta fillmeta = fill.getItemMeta();
+                        ArrayList filllore = new ArrayList<>();
+                        filllore.add("There is nothing here!");
+                        fillmeta.setLore(filllore);
+                        fillmeta.setDisplayName(ChatColor.YELLOW + "There is nothing here!");
+                        fill.setItemMeta(fillmeta);
+                        ItemStack[] menuitems = {die, fill};
+                        killGui.setContents(menuitems);
+                        p.openInventory(killGui);
+                    } else {
+                        p.sendMessage(ChatColor.GREEN + "You do not have the correct perms!");
+                    }
+                } else if (Helpfulgui.getPlugin(Helpfulgui.class).getConfig().getBoolean("killguiperms") == false) {
+                    Inventory killGui = Bukkit.createInventory(p, 9, ChatColor.GREEN + "Kill Gui");
+                    ItemStack die = new ItemStack(Material.NETHERITE_SWORD, 1);
+                    ItemMeta diemeta = die.getItemMeta();
+                    ArrayList dielore = new ArrayList<>();
+                    dielore.add("Kill yourself instantly");
+                    diemeta.setLore(dielore);
+                    diemeta.addEnchant(Enchantment.DAMAGE_ALL, 10, true);
+                    diemeta.setDisplayName(ChatColor.RED + "Die");
+                    die.setItemMeta(diemeta);
+                    ItemStack fill = new ItemStack(Material.GRAY_STAINED_GLASS_PANE, 1);
+                    ItemMeta fillmeta = fill.getItemMeta();
+                    ArrayList filllore = new ArrayList<>();
+                    filllore.add("There is nothing here!");
+                    fillmeta.setLore(filllore);
+                    fillmeta.setDisplayName(ChatColor.YELLOW + "There is nothing here!");
+                    fill.setItemMeta(fillmeta);
+                    ItemStack[] menuitems = {die, fill};
+                    killGui.setContents(menuitems);
+                    p.openInventory(killGui);
 
-
-
-                p.openInventory(killGui);
+                }
             } else {
                 p.sendMessage(ChatColor.translateAlternateColorCodes('&', Helpfulgui.getPlugin(Helpfulgui.class).getConfig().getString("killguidisabledmessage")));
             }
-        }else {
-            System.out.println("Only Players Can Use This Command!");
-        }
 
+            return true;
+        } else {
+            System.out.println("Only players can use this command!");
+        }
         return true;
     }
 }
