d0282b6011315fa0fdc5adb5b79b5f778d02ef59
Max H <maxplaysbest@gmail.com>
2022-08-17 08:32:41 +1200

    Finished kill command hopefully
    Signed-off-by: Max H <49844637+maxherelovescode@users.noreply.github.com>
    Signed-off-by: Max H <maxplaysbest@gmail.com>

--- src/main/java/ml/maxht/helpfulgui/Listeners/killlistener.java
+++ src/main/java/ml/maxht/helpfulgui/Listeners/killlistener.java
@@ -1,11 +1,18 @@
 package ml.maxht.helpfulgui.Listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class killlistener implements Listener {
 
@@ -15,12 +22,59 @@ public class killlistener implements Listener {
         Player p = e.getWhoClicked();
 
         if (e.getView().getTitle().equalsIgnoreCase(ChatColor.RED + "Kill Gui")){
+            Inventory sure = Bukkit.createInventory(p, 9, ChatColor.RED + "Kill GUI: Are You Sure?");
+            ItemStack yes = new ItemStack(Material.GREEN_STAINED_GLASS);
+            ItemMeta yesmeta = yes.getItemMeta();
+            yesmeta.setDisplayName(ChatColor.GREEN + "Yes");
+            ArrayList yeslore = new ArrayList<>();
+            yeslore.add("Clicking Here WILL kill you");
+            yesmeta.setLore(yeslore);
+            yes.setItemMeta(yesmeta);
+            ItemStack no = new ItemStack(Material.RED_STAINED_GLASS);
+            ItemMeta nometa = no.getItemMeta();
+            nometa.setDisplayName(ChatColor.RED + "No");
+            ArrayList nolore = new ArrayList<>();
+            nolore.add("Clicking here will exit the menu");
+            nometa.setLore(nolore);
+            no.setItemMeta(nometa);
+            ItemStack nothing = new ItemStack(Material.BARRIER);
+            ItemMeta nothmeta = nothing.getItemMeta();
+            nothmeta.setDisplayName(ChatColor.YELLOW + "Nothing Here");
+            ArrayList nothlore = new ArrayList<>();
+            nothlore.add("Nothing Here!");
+            nothmeta.setLore(nothlore);
+            no.setItemMeta(nothmeta);
+            sure.setItem(0, yes);
+            sure.setItem(1, yes);
+            sure.setItem(2, yes);
+            sure.setItem(3, yes);
+            sure.setItem(4, nothing);
+            sure.setItem(5, no);
+            sure.setItem(6, no);
+            sure.setItem(7, no);
+            sure.setItem(8, no);
 
             switch (e.getCurrentItem().getType()){
                 case NETHERITE_SWORD:
                     p.closeInventory();
+                    p.openInventory(sure);
+
             }
 
+            e.setCancelled(true);
+        }
+
+        if (e.getView().getTitle().equalsIgnoreCase(ChatColor.RED + "Kill GUI: Are You Sure?")){
+            switch (e.getCurrentItem().getType()){
+                case GREEN_STAINED_GLASS:
+                    p.closeInventory();
+                    p.setHealth(0.0);
+            }
+            switch (e.getCurrentItem().getType()){
+                case RED_STAINED_GLASS:
+                    p.closeInventory();
+                    p.sendMessage(ChatColor.RED + "You have decided not to die");
+            }
             e.setCancelled(true);
         }
 
