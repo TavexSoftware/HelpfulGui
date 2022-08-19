83ac11ae18f0bfc9efc0f94e4e6c5f303abc078b
Max H <maxplaysbest@gmail.com>
2022-08-20 09:23:01 +1200

    Working on tp Command

--- src/main/java/ml/maxht/helpfulgui/Listeners/admin/tpplayer.java
+++ src/main/java/ml/maxht/helpfulgui/Listeners/admin/tpplayer.java
@@ -1,10 +1,15 @@
 package ml.maxht.helpfulgui.Listeners.admin;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class tpplayer implements Listener {
 
-
+    @EventHandler
+    public void tpplayer (InventoryClickEvent e){
+        
+    }
 
 
 }
