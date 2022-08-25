0b65fe755f52f8c99bc817b2b59b3d164f2ead75
Max H <maxplaysbest@gmail.com>
2022-08-25 17:51:27 +1200

    Added bStats

--- src/main/java/ml/maxht/helpfulgui/Helpfulgui.java
+++ src/main/java/ml/maxht/helpfulgui/Helpfulgui.java
@@ -8,6 +8,7 @@ import ml.maxht.helpfulgui.Listeners.admin.stopserver;
 import ml.maxht.helpfulgui.Listeners.admin.tpplayer;
 import ml.maxht.helpfulgui.Listeners.adminlistener;
 import ml.maxht.helpfulgui.Listeners.killlistener;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Helpfulgui extends JavaPlugin {
@@ -28,6 +29,8 @@ public final class Helpfulgui extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new stopserver(), this);
         getCommand("killgui").setExecutor(new killgui());
         getCommand("admin").setExecutor(new admingui());
+        int pluginId = 16263;
+        Metrics metrics = new Metrics(this, pluginId);
         // Do everything else first
         System.out.printf("HelpfulGui Has Started");
         // End Of onEnable
